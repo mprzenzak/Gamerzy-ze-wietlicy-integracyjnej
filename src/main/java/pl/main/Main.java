f6eb44adb285b1b5de4fd90bf2cf7f05ffe184a7
mprzenzak <mprzenzak@gmail.com>
2018-04-19 20:52:55 +0200

    Some changes in project organisation

---
+++ src/main/java/pl/main/Main.java
@@ -0,0 +1,25 @@
+package pl.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/Gameplay.fxml"));
+		StackPane  stackPane = loader.load();
+		Scene scene = new Scene(stackPane, 400, 400);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Gameplay");
+		primaryStage.show();
+	}
+
+}
