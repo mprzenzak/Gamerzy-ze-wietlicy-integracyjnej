25a60019478bb66ee6864a52ba2f381f8f4e912d
mprzenzak <mprzenzak@gmail.com>
2018-04-28 23:23:19 +0200

    New menu with working exit button

--- src/main/java/pl/main/InputSystem.java
+++
@@ -1,5 +0,0 @@
-package pl.main;
-
-public class InputSystem {
-
-}
--- src/main/java/pl/main/Menu.java
+++ src/main/java/pl/main/Menu.java
@@ -1,77 +1,46 @@
 package pl.main;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import java.lang.AutoCloseable;
-
 import javafx.application.Application;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.*;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import javafx.geometry.Pos;
-import javafx.scene.*;
-import javafx.scene.image.*;
-import javafx.scene.paint.*;
-import javafx.scene.shape.*;
-import javafx.scene.text.*;
 
 public class Menu extends Application {
 
-	private static final Node ItemExit = null;
-
-	public Menu(MenuItem menuItem, MenuItem menuItem2, MenuItem menuItem3, MenuItem menuItem4, MenuItem menuItem5,
-			MenuItem menuItem6, MenuItem menuItem7, MenuItem itemExit2) {
-		// TODO Auto-generated constructor stub
-	}
-
-	private Parent createContent() {
-		Pane root = new Pane();
-		root.setPrefSize(720, 480);
-	
-				
-		Title menuTitle = new Title("M A D   J E T P A C K");
-		menuTitle.setTranslateX(75);
-		menuTitle.setTranslateY(200);
-
-		MenuItem itemExit = new MenuItem("EXIT");
-		ItemExit.setOnMouseClicked(event -> System.exit(0));
-		
-		Menu  menu = new Menu(
-				new MenuItem("SINGLEPLAYER"),
-				new MenuItem("MULTIPLAYER"),
-				new MenuItem("FIRST PLAY TOUTORIAL"),
-				new MenuItem("SHOP"),
-				new MenuItem("ACHIVEMENTS"),
-				new MenuItem("HIGHSCORES"),
-				new MenuItem("OPTIONS"),
-				itemExit);
-		menu.setTranslateX(100);
-		menu.setTranslateY(300);
-		
-		root.getChildren().addAll(menuTitle, menu);
-		return root;
-
-	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Scene scene = new Scene(createContent());
-		primaryStage.setTitle("Game menu");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-	
-	private static class MenuItem extends StackPane{
-		public MenuItem(String name) {
-			
-		}
-	}
+	private BorderPane layout;
+	private Scene scene;
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+		layout = new BorderPane();
+		scene = new Scene(layout, 720, 480);
+
+		HBox hbox = new HBox();
+		hbox.setPadding(new Insets(15, 12, 15, 12));
+		hbox.setSpacing(10);
+		hbox.setStyle("-fx-background-color: #F9A825;");
+
+		Button buttonStart = new Button("START GAME");
+		buttonStart.setPrefSize(100, 20);
+		buttonStart.setStyle("-fx-background-color: #E65100;");
+
+		Button buttonExit = new Button("EXIT");
+		buttonExit.setPrefSize(100, 20);
+		buttonExit.setStyle("-fx-background-color: #E65100;");
+
+		buttonExit.setOnMouseClicked(event -> System.exit(0));
+		hbox.getChildren().addAll(buttonStart, buttonExit);
+
+		layout.setCenter(hbox);
+
+		window.setScene(scene);
+		window.show();
 	}
 }
