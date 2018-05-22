d0a5f328084d8f7f7ed4fad3368e5470320c2d35
CarmelWolf <38345053+CarmelWolf@users.noreply.github.com>
2018-05-22 23:24:39 +0200

    Begun implementation of mouse-operated menu because the old idea was hard

--- src/main/java/pl/main/MainMenu.java
+++ src/main/java/pl/main/MainMenu.java
@@ -87,51 +87,37 @@ public class MainMenu {
 			gc.drawImage(playButton, playX, playY);
 			gc.drawImage(shopButtonSelected, shopX, shopY);
 			this.buttonSelected = ButtonSelected.SHOP;
-			delay(300); //fixxxxxxxxxxxxx
 			break;
 
 		case SHOP:
 			gc.drawImage(shopButton, shopX, shopY);
 			gc.drawImage(achievementsButtonSelected, achievementsX, achievementsY);
 			this.buttonSelected = ButtonSelected.ACHIEVEMENTS;
-			delay(300);
 			break;
 
 		case ACHIEVEMENTS:
 			gc.drawImage(achievementsButton, achievementsX, achievementsY);
 			gc.drawImage(highscoresButtonSelected, highscoresX, highscoresY);
 			this.buttonSelected = ButtonSelected.HIGHSCORES;
-			delay(300);
 			break;
 
 		case HIGHSCORES:
 			gc.drawImage(highscoresButton, highscoresX, highscoresY);
 			gc.drawImage(creditsButtonSelected, creditsX, creditsY);
 			this.buttonSelected = ButtonSelected.CREDITS;
-			delay(300);
 			break;
 
 		case CREDITS:
 			gc.drawImage(creditsButton, creditsX, creditsY);
 			gc.drawImage(exitButtonSelected, exitX, exitY);
 			this.buttonSelected = ButtonSelected.EXIT;
-			delay(300);
 			break;
 
 		case EXIT:
 			gc.drawImage(exitButton, exitX, exitY);
 			gc.drawImage(playButtonSelected, playX, playY);
 			this.buttonSelected = ButtonSelected.PLAY;
-			delay(300);
 		}
 	}
-	private void delay(int time) {
-		try {
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
+	
 }
